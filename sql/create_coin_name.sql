5c5111f21299ec75de79582a909616d407cf9ba7
arielyo <ariel.yyl@link8tech.info>
2022-12-02 22:12:45 +0800

    feat: 新增幣別中文資料SQL

---
+++ sql/create_coin_name.sql
@@ -0,0 +1,12 @@
+INSERT INTO coin (code, name, update_time) VALUES ('USD', '美元' ,NOW());
+INSERT INTO coin (code, name, update_time) VALUES ('EUR', '歐元' ,NOW());
+INSERT INTO coin (code, name, update_time) VALUES ('GBP', '英鎊' ,NOW());
+INSERT INTO coin (code, name, update_time) VALUES ('JPY', '日圓' ,NOW());
+INSERT INTO coin (code, name, update_time) VALUES ('TWD', '新台幣' ,NOW());
+INSERT INTO coin (code, name, update_time) VALUES ('CNY', '人民幣' ,NOW());
+INSERT INTO coin (code, name, update_time) VALUES ('VND', '越南盾' ,NOW());
+INSERT INTO coin (code, name, update_time) VALUES ('THB', '銖' ,NOW());
+INSERT INTO coin (code, name, update_time) VALUES ('INR', '印度盧比' ,NOW());
+INSERT INTO coin (code, name, update_time) VALUES ('FRF', '法國法郎' ,NOW());
+INSERT INTO coin (code, name, update_time) VALUES ('DEM', '德國馬克' ,NOW());
+INSERT INTO coin (code, name, update_time) VALUES ('CHF', '瑞士法郎' ,NOW());
